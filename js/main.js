f419c704ff8efaeccb3560a03cdf92dc7c0a4b57
Eli Dupuis <elidupuis@gmail.com>
2013-05-27 20:59:30 -0600

    add underground pedway to map.

--- js/main.js
+++ js/main.js
@@ -9,29 +9,72 @@
     zoomControl: false
   }).setView(center, 14)
 
-  // http://a.tile.stamen.com/toner/12/656/1583.png
-  L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-      maxZoom: 18
-  }).addTo(map);
+
 
   L.control.zoom({ position: 'topright' }).addTo(map);
 
+  // icons
   var icons = {
     lrt_entrance: L.divIcon({className: 'marker lrt-entrance'}),
     user: L.divIcon({className: 'marker user'})
   }
+
+  // tile layers
+  // http://a.tile.stamen.com/toner/12/656/1583.png
+  var tileLayer = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+      maxZoom: 18
+  }).addTo(map);
+
+  // overlay layers
+  var pedwayLayer = L.layerGroup().addTo(map);
+  var entrancesLayer = L.layerGroup().addTo(map);
+  var overlayMaps = {
+    "Pedway": pedwayLayer,
+    "LRT Entrances": entrancesLayer
+  }
+  L.control.layers(null, overlayMaps, { position: 'bottomright' }).addTo(map);
+
+  // main street level LRT entrances
   $.getJSON('data/lrt-entrances.json', function(data){
     lrt_entrances = data.elements
     $.each(lrt_entrances, function(i, item) {
-      console.log(item)
+      // console.log(item)
       var tags = item.tags || {}
-      var marker = L.marker([item.lat, item.lon], { icon: icons.lrt_entrance }).addTo(map);
-      marker.bindPopup('<b>' + tags.name + '</b>')
+      var marker = L.marker([item.lat, item.lon], { icon: icons.lrt_entrance }).addTo(entrancesLayer);
+      if (tags.name) {
+        marker.bindPopup('<b>' + tags.name + '</b>')
+      }
       markers.push(marker)
     })
   })
 
+  // underground pedway footpaths
+  $.getJSON('data/pedway-underground.json', function(data){
+    var pedway = data.elements
+    _.each(pedway, function(element){
+
+      // for each way
+      if (element.type === 'way') {
+        // loop through node references
+        var latlngs = _.map(element.nodes, function(id){
+          // find associated node in order to return latlng
+          var node = _.find(pedway, function(n){
+            return n.id === id
+          })
+          return [node.lat, node.lon]
+        })
+
+        // add pedway section to pedway layer.
+        // use MultiPolyline instead?
+        var polyline = L.polyline(latlngs).addTo(pedwayLayer);
+
+      }
+
+    })
+  })
+
+  // geolocation
   map.locate({
     watch: true,
     enableHighAccuracy: true
